4e94eae457900454028d175fea0629dffe8043e1
Pham Xuan Truong <truongvn0101@gmail.com>
2020-07-14 19:30:33 +0700

    nothing

--- btvn/bai4.js
+++ btvn/bai4.js
@@ -3,9 +3,9 @@ let to100, to50, to20,to10;
 to100= x / 100000; // so to 100k
 let du1 = x%100000
 to50 = du1 / 50000;// so to 50k
-let du2 = x%50000;
+let du2 = du1%50000;
 to20 = du2 / 20000;// so to 20k
-let du3 = x%20000;
+let du3 = du2%20000;
 to10 = du3 /10000; // so to 10k
 console.log("ta doi",x,"thanh ",parseInt(to100),"to 100k",parseInt(to50),"to 50k",parseInt(to20),"to 20k",parseInt(to10),"to 10k")
 
